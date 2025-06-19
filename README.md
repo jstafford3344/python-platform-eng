b3f95b1be478bed89731982bb1ec2e474db96453
jstafford3344 <jstafford@getsporttrade.com>
2025-06-19 10:52:59 -0400

    chore: initial commit

--- README.md
+++
@@ -1,3 +0,0 @@
-# Platform Engineering
-- The goal of platform engineering is to create an IDP (Internal Developer Platform) for developers to be able to do self-service tasks to build, deploy and manage their applications.# python-platform-eng
-# python-platform-eng
---
+++ src/app.py
@@ -0,0 +1,19 @@
+from flask import Flask, jsonify
+import datetime
+import socket
+
+app = Flask(__name__)
+
+@app.route('/api/v1/details')
+def get_details():
+    return jsonify({
+        "time": datetime.datetime.now(),
+        "hostname": socket.gethostname()
+    })
+
+@app.route('/api/v1/healthz')
+def health():
+    return jsonify({"status": "UP"}), 200
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0")
