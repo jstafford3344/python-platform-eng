dde42049875a9fa49d85bfc6365dbe2f7d128645
jstafford3344 <jstafford@getsporttrade.com>
2025-06-22 12:45:42 -0400

    chore: changing endpoint

--- src/app.py
+++ src/app.py
@@ -4,12 +4,13 @@ import socket
 
 app = Flask(__name__)
 
-@app.route('/api/v1/details')
-def get_details():
+@app.route('/api/v1/info')
+def get_info():
     return jsonify({
         "time": datetime.datetime.now(),
         "hostname": socket.gethostname(),
-        "msg": "Triggering Github Actions run"
+        "msg": "Triggering Github Actions run",
+        "deployed_on": "kubernetes"
     })
 
 @app.route('/api/v1/healthz')
